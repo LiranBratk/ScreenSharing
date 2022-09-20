6c16fb048199410607e02154042417ec18890ea9
LiranBar <bargigi78@gmail.com>
2022-09-20 18:00:27 +0300

    ש

--- index.js
+++ index.js
@@ -58,4 +58,49 @@ app.listen(process.env.PORT || 3000, function () {
     );
 });
 
+const { Server } = require('socket.io');
+const io = new Server(app);
+let connectedUsers = [];
+let user_name = [];
+
+io.on('connection', (socket) => {
+    // It's necessary to socket knows all clients connected
+    connectedUsers.push(socket.id);
+
+    // Emit to myself the other users connected array to start a connection with each them
+    console.log(connectedUsers.join(', '), socket.id);
+    let adminSocket = connectedUsers[0]
+    const otherUsers = (adminSocket == socket.id) ? [] : [adminSocket] //connectedUsers.filter(socketId => socketId !== socket.id);
+    socket.emit('other-users', otherUsers);
+
+    // Send Offer To Start Connection
+    socket.on('appendname', (adminSocket, name) => {
+        user_name.push(name)
+        let socketID = connectedUsers[connectedUsers.length - 1];
+        console.log(connectedUsers[connectedUsers.length - 1], user_name[user_name.length - 1])
+        socket.to(adminSocket).emit("appendname", socketID, name)
+    });
+
+    // Send Offer To Start Connection
+    socket.on('offer', (socketId, description) => {
+        socket.to(socketId).emit('offer', socket.id, description, socketId);
+    });
+
+    // Send Answer From Offer Request
+    socket.on('answer', (socketId, description) => {
+        socket.to(socketId).emit('answer', description);
+    });
+
+    // Send Signals to Establish the Communication Channel
+    socket.on('candidate', (socketId, candidate) => {
+        socket.to(socketId).emit('candidate', candidate);
+    });
+
+    // Remove client when socket is disconnected
+    socket.on('disconnect', () => {
+        socket.to(adminSocket).emit("destroyed", socket.id)
+        connectedUsers = connectedUsers.filter(socketId => socketId !== socket.id);
+    });
+});
+
 module.exports = app;
